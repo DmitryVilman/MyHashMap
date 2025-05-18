bc6971f1d034df1e6c58646fa830ece8b1905c39
DmitryVilman <DmitryVilman@gmail.com>
2025-05-18 11:50:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<String, Integer> bools = new MyHashMap<>();
-        bools.put("Adolf", 10);
-        System.out.println(bools.get("Adolf"));
+        MyHashMap<String, Integer> list = new MyHashMap<>();
+        list.put("Dmitry", 13);
+        list.put("Anton",2);
+        System.out.println(list.get("Dmitry"));
+        System.out.println(list.get("Anton"));
+        list.remove("Anton");
+        System.out.println(list.get("Anton"));
     }
 
 }
